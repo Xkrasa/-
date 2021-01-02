389b24ac8fc6ed0227787015c13b01a078d77734
Xkrasa <73265688+Xkrasa@users.noreply.github.com>
2021-01-02 23:06:44 +0800

    Add files via upload

--- 评分.py
+++ 评分.py
@@ -1,25 +1,40 @@
-try:
-    rs=int(input())
-    if rs>2:
+f,rs=0,0
+def chojiang():
+    global f,rs
+    if f==0:
+        print("请输入评委人数：")
+        try:
+            rs=int(input())
+            if rs>2:
+                f=1
+                chojiang()
+            else:
+                print("评委人数不够")
+                chojiang()
+        except ValueError:
+            print("请输入数字")
+            chojiang()
+    else:
         g=[]
         print("接下来需要输入",rs,"个分数")
         while rs>0:
             try:
                 sr=int(input())
-                if sr>=0&sr<=100:
-                    g.append()
+                if (sr>=0)&(sr<=100):
+                    g.append(sr)
                     rs-=1
                     if rs!=0:
                         print("还有",rs,"个评分待输入")
+                    else:
+                        break
                 else:
-                    break
+                    print("输入分数超出范围,请输入在[0,100]之内的数")
+                    continue
             except:
-                print("输入分数超出范围,请输入在[0,100]之内的数")
+                print("请输入数字")
         ma=max(g)
         mi=min(g)
         eg=(sum(g)-ma-mi)/(len(g)-2)
         print("选手的最终成绩为:",eg)
-    else:
-        print("评委人数不够")
-except ValueError:
-    print("输入非数字")
+        f=0
+chojiang()
